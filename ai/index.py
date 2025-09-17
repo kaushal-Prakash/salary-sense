0511817da417907ba8402893790167d572968342
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-09-17 19:16:16 +0530

    added cors

--- ai/index.py
+++ ai/index.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from routes.ai_route import ai_router
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],            # allow all HTTP methods
+    allow_headers=["*"],            # allow all headers
+)
 
 #routes
 app.include_router(ai_router, prefix="/ai")
